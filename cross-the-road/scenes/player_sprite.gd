b593cb4b5a3a6bb49487bea3ea193be1907e0137
George Whittington <lwhittington2231@gmail.com>
2025-12-27 19:02:56 -0500

    Basic character movement

--- cross-the-road/scenes/player_sprite.gd
+++ cross-the-road/scenes/player_sprite.gd
@@ -1,7 +1,7 @@
 extends Sprite2D
 
 
-var direction: Vector2 = Vector2.RIGHT
+var direction: Vector2 = Vector2.ZERO
 var speed: int = 200
 
 
@@ -10,6 +10,28 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("exercise"):
+		print("Only Once")
+
+
 func _physics_process(delta: float) -> void:
-	# grabs the position of the node attached to it
+	# set back to (0, 0) each frame
+	#direction = Vector2.ZERO
+	# basic movement vector based on checking each input manually
+	#if Input.is_action_pressed("up"):
+		#direction.y -= 1
+	#if Input.is_action_pressed("down"):
+		#direction.y += 1
+	#if Input.is_action_pressed("left"):
+		#direction.x -= 1
+	#if Input.is_action_pressed("right"):
+		#direction.x += 1
+	
+	# normalize vector so diagnonal movement is not fasterxz
+	#position += direction.normalized() * speed * delta
+	
+	# assign Input Mapping to each direction, also normalizes itself
+	direction = Input.get_vector("left", "right", "up", "down")
+	
 	position += direction * speed * delta
