7974ea4e8f700e3d1c80b815f38446ea27be062e
George Whittington <lwhittington2231@gmail.com>
2025-12-29 23:12:15 -0500

    Added world boarders to the game to make the player stay in the window

--- cross-the-road/scenes/player.gd
+++ cross-the-road/scenes/player.gd
@@ -1,8 +1,8 @@
-extends Node2D
+extends CharacterBody2D
 
 
 var direction: Vector2 = Vector2.ZERO
-var speed: int = 200
+var speed: int = 100
 
 
 # Called when the node enters the scene tree for the first time.
@@ -15,23 +15,9 @@ func _process(_delta: float) -> void:
 		print("Only Once")
 
 
-func _physics_process(delta: float) -> void:
-	# set back to (0, 0) each frame
-	#direction = Vector2.ZERO
-	# basic movement vector based on checking each input manually
-	#if Input.is_action_pressed("up"):
-		#direction.y -= 1
-	#if Input.is_action_pressed("down"):
-		#direction.y += 1
-	#if Input.is_action_pressed("left"):
-		#direction.x -= 1
-	#if Input.is_action_pressed("right"):
-		#direction.x += 1
-	
-	# normalize vector so diagnonal movement is not fasterxz
-	#position += direction.normalized() * speed * delta
-	
+func _physics_process(_delta: float) -> void:	
 	# assign Input Mapping to each direction, also normalizes itself
 	direction = Input.get_vector("left", "right", "up", "down")
 	
-	position += direction * speed * delta
+	velocity = direction * speed
+	move_and_slide()
