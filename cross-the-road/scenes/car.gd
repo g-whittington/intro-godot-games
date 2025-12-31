6a169456a3cd9b8c66082e22023e86887edaa59a
George Whittington <lwhittington2231@gmail.com>
2025-12-30 20:45:34 -0500

    Cars spawn at markers

---
+++ cross-the-road/scenes/car.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+# movement vars for the car instance
+var direction : Vector2
+var speed := 100
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+
+func _ready() -> void:
+	# sets direction based on spawn position
+	if position.x < 0:
+		direction = Vector2.RIGHT
+		sprite_2d.flip_h = true
+	else:
+		direction = Vector2.LEFT
+
+func _process(delta: float) -> void:
+	# car should always move
+	position += direction * speed * delta
+	
+	# should delete when off screen
+	if position.x < -250 or position.x > 250:
+		queue_free()
--- cross-the-road/scenes/game.gd
+++ cross-the-road/scenes/game.gd
@@ -1,6 +1,25 @@
 extends Node2D
 
+# preloaded using the uid
+const CAR: PackedScene = preload("uid://cik0suxpxidrw")
+
+# Container to hold all spawned cars
+@onready var active_cars: Node2D = $CarSystem/ActiveCars
+@onready var car_start_positions: Node2D = $CarSystem/CarStartPositions
+
 
 func _on_win_zone_body_entered(body: Node2D) -> void:
 	if body is Player:
-		print("You Win!")
+		print("You Win!") 
+
+
+func _on_car_spawn_timer_timeout() -> void:
+	# create new car object
+	var car: Area2D = CAR.instantiate()
+	
+	# randomly choose the car start position
+	var start_pos: Marker2D = car_start_positions.get_children().pick_random()
+	car.position = start_pos.position
+	
+	active_cars.add_child(car)
+	
