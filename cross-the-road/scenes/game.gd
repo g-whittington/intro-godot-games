35d8bac011e291367200e49f5ff165816c3f44a6
George Whittington <lwhittington2231@gmail.com>
2025-12-30 21:24:45 -0500

    Set up collision layers and masks

--- cross-the-road/scenes/game.gd
+++ cross-the-road/scenes/game.gd
@@ -21,5 +21,10 @@ func _on_car_spawn_timer_timeout() -> void:
 	var start_pos: Marker2D = car_start_positions.get_children().pick_random()
 	car.position = start_pos.position
 	
+	# connect the body_entered signal in code
+	car.body_entered.connect(go_to_title)
+	
 	active_cars.add_child(car)
 	
+func go_to_title(_body: Node2D) -> void:
+	print("collide ", str(_body))
